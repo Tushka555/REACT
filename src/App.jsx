b319a8057957c5dcad0148218f28f42d0d2d1e6d
24LP2767 24LP2767 <24LP2767@G301-CC0194.local>
2025-01-02 17:47:47 +0800

    yhh

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
  
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1)
   return (
     <>
       <div>
@@ -17,7 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count)=> count *10)}>count is {count}</button>
+        <button onMouseMove={() => setCount((count)=> count * 2)}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
